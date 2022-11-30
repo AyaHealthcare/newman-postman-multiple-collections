2b714dd03a158338b61e0d4bce059fb9082eb3b0
Tuyen Nguyen <trongtuyen96@gmail.com>
2022-12-01 00:15:59 +0200

    Add covers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const newmanConfig = require('./src/main.js');
 
 // Validate input arguments
 if (process.argv.length <= 2) {
-    console.log("\x1b[31m==== PLEASE PROVIDE PATH TO FEED FILE ====\nEx: '../feeds/sample_feed.json'\x1b[0m")
+    console.log("\x1b[31mPlease provide path to the feed file\nEx: '../feeds/feed.json'\x1b[0m")
 } else {
     var args = process.argv.slice(2);
     new newmanConfig(args[0]);
